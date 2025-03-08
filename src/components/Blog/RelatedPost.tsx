22fec2c00b65cd227df2597d9c3d739424a84916
Manish sonawane <manishsonawane3010@gmail.com>
2025-03-08 14:10:20 +0530

    Image in the Post

--- src/components/Blog/RelatedPost.tsx
+++ src/components/Blog/RelatedPost.tsx
@@ -20,7 +20,7 @@ const RelatedPost = ({
     <div className="flex items-center lg:block xl:flex">
       <div className="mr-5 lg:mb-3 xl:mb-0">
         <div className="relative h-[60px] w-[70px] overflow-hidden rounded-md sm:h-[75px] sm:w-[85px]">
-          <Image src={image} alt={title} fill />
+          <Image src={image} alt={title} fill className="object-cover" />
         </div>
       </div>
       <div className="w-full">
